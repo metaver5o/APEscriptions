adb23f7f0e4a6ac40e48b825d554f5c857b25d3d
metaver5o.eth <marcomatos@google.com>
2024-10-24 11:55:04 -0300

    cleaning main

--- data/hex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# decode your hexdata from chain transaction
-# Hex data
-hex_data="replace with your full hex string"
-
-# Convert hex to binary and write to a file
-echo $hex_data | xxd -r -p > output_image.png
-
-echo "Image successfully saved as output_image.png"
